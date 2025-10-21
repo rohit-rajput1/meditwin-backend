47626c95fbaec4eded0d749cae482a9d97bd1f07
rohit-rajput1 <rohitt31rajput2001@gmail.com>
2025-10-21 21:49:36 +0530

    relation added between user_id and report_type_id

--- database/models/report.py
+++ database/models/report.py
@@ -9,34 +9,33 @@ Defines the Report model for a PostgreSQL database using SQLAlchemy ORM.
 - Related to Chat model for discussions on the report.
 """
 
-from sqlalchemy import String, DateTime, ForeignKey, Enum, JSON
+from sqlalchemy import JSON, DateTime, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from datetime import datetime, timezone
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 import uuid
+from datetime import datetime, timezone
 from database.base import Base
-from .enumconstant import ReportTypeEnum
+from .report_type import ReportType
 
 class Report(Base):
     __tablename__ = "report"
 
-    # Primary key UUID
     report_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    
-    # Foreign key linking to User
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("user.user_id"))
 
-    # Report details
-    report_type: Mapped[ReportTypeEnum] = mapped_column(Enum(ReportTypeEnum))
-    report_file_url: Mapped[str] = mapped_column(String)
-    summary: Mapped[dict] = mapped_column(JSON)
-    key_findings: Mapped[dict] = mapped_column(JSON)
-    insights: Mapped[dict] = mapped_column(JSON)
-    recommendations: Mapped[dict] = mapped_column(JSON)
+    # Instead of Enum, we use a ForeignKey
+    report_type_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("report_type.report_type_id"))
 
-    # Upload timestamp
+    report_file_url: Mapped[str] = mapped_column(String,nullable=True)
+    summary: Mapped[dict] = mapped_column(JSON, nullable=True)
+    key_findings: Mapped[dict] = mapped_column(JSON, nullable=True)
+    insights: Mapped[dict] = mapped_column(JSON, nullable=True)
+    recommendations: Mapped[dict] = mapped_column(JSON, nullable=True)
+
+
     uploaded_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(timezone.utc))
 
     # Relationships
-    user = relationship("User", back_populates="reports")  # Each report belongs to a user
-    chats = relationship("Chat", back_populates="report", lazy="dynamic")  # One-to-many chats
+    user = relationship("User", back_populates="reports")
+    chats = relationship("Chat", back_populates="report", lazy="dynamic")
+    report_type = relationship("ReportType", back_populates="reports")
