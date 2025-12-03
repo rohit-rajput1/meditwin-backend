6081cd026671fbe0329dc23610e9b777920601cf
rohit-rajput1 <rohitt31rajput2001@gmail.com>
2025-12-03 21:49:47 +0530

    relatipnship adding

--- database/models/report_type.py
+++ database/models/report_type.py
@@ -1,17 +1,21 @@
-# report_type.py
 import uuid
 from sqlalchemy import String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from database.base import Base  # or wherever your Base is
+from database.base import Base
+
 
 class ReportType(Base):
     __tablename__ = "report_type"
 
     report_type_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
     )
+
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
 
-    # Relationship to reports
     reports = relationship("Report", back_populates="report_type")
+
+    dashboards = relationship("Dashboard", back_populates="report_type")
