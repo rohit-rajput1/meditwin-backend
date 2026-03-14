bb13e4ecf4e9d2a9007fa11df9fe69f42b923fc7
rohit-rajput1 <rohit31rajput2001@gmail.com>
2026-03-14 17:23:27 +0530

    type model formatting

--- database/models/report_type.py
+++ database/models/report_type.py
@@ -8,14 +8,7 @@ from database.base import Base
 class ReportType(Base):
     __tablename__ = "report_type"
 
-    report_type_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        default=uuid.uuid4,
-    )
-
+    report_type_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-
     reports = relationship("Report", back_populates="report_type")
-
     dashboards = relationship("Dashboard", back_populates="report_type")
